4bb0e92a589faba9ed504f4c001fb5410aa61070
Danila <msabirova34@gmail.com>
2022-03-11 19:28:08 +0300

    finish 2 page

---
+++ src/assets/js/app.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 15,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+    1360: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+})
