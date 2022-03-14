1f93ca598342bdc95edbd878178c70d4842f1cba
Danila <msabirova34@gmail.com>
2022-03-14 13:03:39 +0300

    finish 4 pages

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -36,3 +36,15 @@ const swiper = new Swiper('.swiper', {
     },
   },
 })
+// End of Slider
+
+const iconMenu = document.querySelector('.header__icon')
+const menuBody = document.querySelector('.header__menu')
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock')
+    iconMenu.classList.toggle('_active')
+    menuBody.classList.toggle('_active')
+  })
+}
+// End of Burger
