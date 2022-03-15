0f4df0970553091850b6ce9a93d85f60ac3fda48
Danila <msabirova34@gmail.com>
2022-03-15 13:12:28 +0300

    complete

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,28 +1,15 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-
-  slidesPerView: 1,
+  slidesPerView: 1.1,
   spaceBetween: 15,
 
   breakpoints: {
-    640: {
+    520: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
